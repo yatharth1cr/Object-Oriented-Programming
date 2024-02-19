3a99576c37159d9909e10e446073848f9244eb53
yatharth1cr <yatharthgiri187@gmail.com>
2024-02-19 10:09:28 +0530

    completed with the assignment of oop

--- Assignment-ObjectCreation/script.js
+++ Assignment-ObjectCreation/script.js
@@ -1,40 +1,55 @@
 // Without Object
-let title = 'Where is the capital of Jordan'
-let options = ['Tashkent', 'Amaan', 'Kuwait City', 'Nairobi']
-let correctAnswerIndex = 1
+let title = "Where is the capital of Jordan";
+let options = ["Tashkent", "Amaan", "Kuwait City", "Nairobi"];
+let correctAnswerIndex = 1;
 function isAnswerCorrect(index) {
-    return index === correctAnswerIndex;
+  return index === correctAnswerIndex;
 }
-function getCorrectAnswer(index) {
-    return options[index]
+function getCorrectAnswer() {
+  return options[correctAnswerIndex];
 }
 
 // Organize using object
 let question1 = {
-    title: 'where is the capital of Jordan',
-    options: ['Tashkent', 'Amaan', 'Kuwait City', 'Nairobi'],
-    correctAnswerIndex: 1,
-    isAnswerCorrect: function (index) {
-        return index === correctAnswerIndex;
-    },
-    getCorrectAnswer: function (index) {
-        return question1.options[index];
-    },
-}
+  title: "where is the capital of Jordan",
+  options: ["Tashkent", "Amaan", "Kuwait City", "Nairobi"],
+  correctAnswerIndex: 1,
+  isAnswerCorrect: function (index) {
+    return index === correctAnswerIndex;
+  },
+  getCorrectAnswer: function () {
+    return question1.options[correctAnswerIndex];
+  },
+};
 
 // Use a function to create object
 function createQuestion(title, options, correctAnswerIndex) {
-    let question = {};
-    question.title = title;
-    question.options = options;
-    question.correctAnswerIndex = correctAnswerIndex;
-    question.isAnswerCorrect = function (index) {
-        return index === question.correctAnswerIndex;
-    };
-    question.getCorrectAnswer = function (index) {
-        return question.options[index]
-    }
+  let question = {};
+  question.title = title;
+  question.options = options;
+  question.correctAnswerIndex = correctAnswerIndex;
+  question.isAnswerCorrect = function (index) {
+    return index === question.correctAnswerIndex;
+  };
+  question.getCorrectAnswer = function () {
+    return question.options[correctAnswerIndex];
+  };
+  return question;
 }
 
 // Convert the function to use this keyword
+function createQuestion(title, options, correctAnswerIndex) {
+  let question = {};
+  question.title = title;
+  question.options = options;
+  question.correctAnswerIndex = correctAnswerIndex;
+  question.isAnswerCorrect = function (index) {
+    return index === this.correctAnswerIndex;
+  };
+  question.getCorrectAnswer = function () {
+    return this.options[this.correctAnswerIndex];
+  };
+  return question;
+}
+
 // Write test by creating two objects also test both methods.
